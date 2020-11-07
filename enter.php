e10f463d98b5a05686fe861ab5d1b17ca110ad54
Leo6Leo <36619969+Leo6Leo@users.noreply.github.com>
2020-11-07 12:40:41 -0500

    Update enter.php

--- enter.php
+++ enter.php
@@ -6,40 +6,40 @@
 </head>  
 <body>  
     <?php   
-        session_start();//��¼ϵͳ����һ��session����  
-        $username=$_REQUEST["mailnum"];//��ȡhtml�е��û�����ͨ��post����  
-        $password=$_REQUEST["password"];//��ȡhtml�е����루ͨ��post����  
+        session_start();//start session
+        $username=$_REQUEST["mailnum"];// store the username
+        $password=$_REQUEST["password"];//get the password
 		$behave='login';
   
         $con=mysql_connect("localhost","sixbox","sixboxsixbox");  
         if (!$con) {  
-            die('连接失败'.$mysql_error());  
+            die('connection failed'.$mysql_error());  
         }  
-        mysql_select_db("xdm447914479_db",$con);//use user_info���ݿ⣻  
+        mysql_select_db("xdm447914479_db",$con);//select the db
         $dbmailnum=null;  
         $dbpassword=null;  
-        $result=mysql_query("select * from 6boxesuser where username ='{$username}' and isdelete =0 ");//�����Ӧ�û�������Ϣ��isdelete��ʾ�����ݿ��ѱ�ɾ��������  
+        $result=mysql_query("select * from 6boxesuser where username ='{$username}' and isdelete =0 ");//determine use is valid or not
 		 
-        while ($row=mysql_fetch_array($result)) {//whileѭ����$result�еĽ���ҳ���  
+        while ($row=mysql_fetch_array($result)) {//search in the database
             $dbmailnum=$row["username"];  
             $dbpassword=$row["password"];  
 		
 			
 			}  
 		        
-        if (is_null($dbmailnum)) {//�û��������ݿ��в�����ʱ����index.html����  
+        if (is_null($dbmailnum)) {//If mail does not exsit, then return the following info
     ?>  
     <script type="text/javascript">  
-        alert("账号不存在或被限制登录！Account banned or not exist.(╥﹏╥)o");  
+        alert("Account banned or not exist.(╥﹏╥)o");  
         window.location.href="../login-1.html";  
     </script>  
     <?php   
         }  
         else {  
-            if ($dbpassword!=$password){//����Ӧ���벻��ʱ����index.html����  
+            if ($dbpassword!=$password){//If password wrong, return the following info
     ?>  
     <script type="text/javascript">  
-        alert("账号或密码错误，检查一下吧 Wrong password or e-mail");  
+        alert("Wrong password or e-mail.");  
         window.location.href="../login-1.html";  
     </script>  
     <?php   
@@ -51,7 +51,7 @@
 				$ip=$_SERVER['REMOTE_ADDR'];
                 $_SESSION["code"]=mt_rand(0, 100000);//welcome.php  
 				setcookie("username",$username,time()+60*60*24*30);
-				mysql_query("insert into 6boxeslog (username,method,behave,ip) values('{$username}','{$method}','{$behave}','{$ip}')") or die("链接出错".mysql_error()) ;  
+				mysql_query("insert into 6boxeslog (username,method,behave,ip) values('{$username}','{$method}','{$behave}','{$ip}')") or die("Connection failed".mysql_error()) ;  
     ?>  
     <script type="text/javascript">  
         window.location.href="../vocab.html";  
@@ -59,7 +59,7 @@
     <?php   
             }  
         }  
-    mysql_close($con);//关闭连接 
+    mysql_close($con);//connection closed
     ?>  
 </body>  
 </html>  
